a0561935b222bc033180a01d9617c82e12812839
AndreyMinyay1o <113465922+AndreyMinyay1o@users.noreply.github.com>
2023-05-16 20:51:23 +0300

    Update index.php

--- 4/index.php
+++ 4/index.php
@@ -209,8 +209,8 @@ else {
   $bio= $_POST['bio'];
 
   // Сохранение в БД.
-$user = 'u52927';
-$pass = '5758562';
+$user = 'u52968';
+$pass = '8835740';
   $db = new PDO('mysql:host=localhost;dbname=u52927', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
    try {
     $stmt = $db->prepare("INSERT INTO form SET name=:name, email=:email, year=:byear, pol=:pol, limbs=:limbs, bio=:bio");
